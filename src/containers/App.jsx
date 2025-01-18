68f56f894f255bb9c77fc44fa2d20515cf275368
Jose David Ramirez <117590011+jdramirezu@users.noreply.github.com>
2025-01-18 14:54:18 -0500

    removed old code

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,38 +5,23 @@ import './App.css';
 import Scroll from '../components/Scroll.jsx';
 
 const App = () =>{
-    // constructor(){
-    //     super();
-    //     this.state = {
-    //         robots: [],
-    //         searchfield: ''
-    //     } 
-    // }
     const [robots, setRobots] = useState([]);
     const [searchfield, setSearchfield] = useState('');
-    let i = 1;
+
     useEffect(() => {
-        
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => setRobots(users));
-        console.log("useEffect ran " + (i++));
     },[]);
-    // componentDidMount(){
-    //     fetch('https://jsonplaceholder.typicode.com/users')
-    //     .then(response => response.json())
-    //     .then(users => this.setState({robots: users}));
-    // }
 
     const onSearchChange = (event) =>{
         setSearchfield(event.target.value);
     }
 
-    // const {robots, searchfield} = this.state;
     const filteredRobots = robots.filter(robot =>{
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    console.log(robots, searchfield);
+    
     return !robots.length ?
         <h1 className='tc'>Loading</h1> :
     (
